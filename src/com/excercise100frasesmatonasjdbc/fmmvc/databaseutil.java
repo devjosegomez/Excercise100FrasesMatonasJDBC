af032cccf7f15848a88ce889e4ba0aaf154984c8
devjosegomez <devjosegomez@gmail.com>
2019-11-19 15:25:58 -0600

    Added timezone

--- src/com/excercise100frasesmatonasjdbc/fmmvc/databaseutil.java
+++ src/com/excercise100frasesmatonasjdbc/fmmvc/databaseutil.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class databaseutil {
-	private static final String URL_FORMAT = "jdbc:%s://%s:%s/%s";
+	private static final String URL_FORMAT = "jdbc:%s://%s:%s/%s?%s";
 	
 	public static Connection getConnection() {
 		String dbms = "mysql";
@@ -12,7 +12,10 @@ public class databaseutil {
 		String dataBase = "frases_matonas";
 		String user = "root";
 		String pass = "root";
-		String url = String.format(URL_FORMAT, dbms, host, port, dataBase);
+		String timeZone = "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String url = String.format(URL_FORMAT, dbms, host, port, dataBase, timeZone);
+		
+		System.out.println(url);
 		
 		Connection connection = null;
 		
