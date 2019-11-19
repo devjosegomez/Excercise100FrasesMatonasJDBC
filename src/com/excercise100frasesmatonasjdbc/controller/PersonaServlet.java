8d9d42100092a7fac77225297ac63a72c9c9c46b
devjosegomez <devjosegomez@gmail.com>
2019-11-19 15:58:01 -0600

    Added PersonaServler servlet

--- src/com/excercise100frasesmatonasjdbc/controller/PersonaServlet.java
+++ src/com/excercise100frasesmatonasjdbc/controller/PersonaServlet.java
@@ -1,7 +1,34 @@
 package com.excercise100frasesmatonasjdbc.controller;
 
-public class PersonaServlet {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PersonaServlet
+ */
+@WebServlet("/PersonaServlet")
+public class PersonaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PersonaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String txtNombre = req.getParameter("txtNombre");
+		String txtEdad = req.getParameter("txtEdad");
+		
+	}
+
 }
