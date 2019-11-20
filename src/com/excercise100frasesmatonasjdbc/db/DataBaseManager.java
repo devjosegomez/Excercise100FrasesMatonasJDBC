4804cee70a12223832a68588af0abb4bbb57c756
devjosegomez <devjosegomez@gmail.com>
2019-11-20 12:37:13 -0600

    Added crearPersona(), crearFrase()

--- src/com/excercise100frasesmatonasjdbc/db/DataBaseManager.java
+++ src/com/excercise100frasesmatonasjdbc/db/DataBaseManager.java
@@ -2,11 +2,22 @@ package com.excercise100frasesmatonasjdbc.db;
 
 import java.sql.Connection;
 
+import com.excercise100frasesmatonasjdbc.model.Frase;
+import com.excercise100frasesmatonasjdbc.model.Persona;
+
 public class DataBaseManager {
 	private Connection conn;
 	
 	public DataBaseManager(Connection conn) {
 		this.conn = conn;
+	}
+	
+	public void crearPersona(Persona persona) {
+		
+	}
+	
+	public void crearFrase(Frase frase) {
+		
 	}
 	
 	
